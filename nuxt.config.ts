3e11544834830e88b42d1b506ae8e7e328a4a7a5
ClouDGmUz <cloudgmuz@mgial.com>
2025-03-06 09:52:41 +0500

    i did mistake

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  
+  vite: {
+    server: {
+      allowedHosts: [
+        '', // Your Domain Here
+        'localhost'
+      ]
+    }
+  },
+
+  // Optional: also set Nuxt's dev server config
+  devServer: {
+    host: '0.0.0.0',
+    port: 3001
+  }
 })
