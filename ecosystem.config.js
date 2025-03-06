1ec0a2a4dca01ca2f2b762c39425e956d853e977
ClouDGmUz <cloudgmuz@mgial.com>
2025-03-06 09:40:50 +0500

    fixed errorss

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 export default {
   apps: [{
     name: 'taplinknuxt',
-    script: 'npm',
-    args: 'run start',
+    script: './node_modules/nuxt/bin/nuxt.mjs',
+    args: 'start',
     cwd: __dirname,
     watch: false,
     autorestart: true,
