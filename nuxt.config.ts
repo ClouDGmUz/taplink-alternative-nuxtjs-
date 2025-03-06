f818de52b5b08c41ea8ad0b6595c99055c8e9269
ClouDGmUz <cloudgmuz@mgial.com>
2025-03-06 15:25:07 +0500

    hotfix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   
   app: {
     head: {
-      title: 'DEW Motor Oil - Premium Quality Engine Oils | DEW Motor Moyi',
+      // Add CSP meta tag
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -17,37 +17,15 @@ export default defineNuxtConfig({
         { property: 'og:type', content: 'website' },
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'DEW Motor Oil - Premium Quality Engine Oils | DEW Motor Moyi' },
-        { name: 'twitter:description', content: 'DEW motor oil - Specializing in manufacturing and selling high-quality automotive oils. Our experts pay special attention to every detail to ensure reliable protection of your car engine. | DEW motor moyi - avtomobillar uchun sifatli moylar ishlab chiqarish va sotishga ixtisoslashgan kompaniya.' }
+        { name: 'twitter:description', content: 'DEW motor oil - Specializing in manufacturing and selling high-quality automotive oils. Our experts pay special attention to every detail to ensure reliable protection of your car engine. | DEW motor moyi - avtomobillar uchun sifatli moylar ishlab chiqarish va sotishga ixtisoslashgan kompaniya.' },
+        { 
+          'http-equiv': 'Content-Security-Policy',
+          content: "default-src 'self' dew.condoroil.uz; img-src 'self' data:; media-src 'self'"
+        }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        // Add this line to fix preload warning
-        { rel: 'preload', href: '/_nuxt/builds/meta/dev.json', as: 'fetch', crossorigin: 'anonymous' }
       ]
-    }
-  },
-
-  vite: {
-    server: {
-      hmr: {
-        protocol: 'wss',
-        host: '', // Replace with your server's hostname
-        port: 5173
-      }
-    }
-  },
-
-  // Optional: also set Nuxt's dev server config
-  devServer: {
-    host: '0.0.0.0',
-    port: 5173
-  },
-  runtimeConfig: {
-    awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    public: {
-      awsRegion: process.env.AWS_REGION,
-      awsBucket: process.env.AWS_BUCKET_NAME
     }
   }
 })
